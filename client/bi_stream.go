c823ee8ad4c46bc52f719c6338ccc890d38b6162
Tejasvi <tejasvi.tanwar541@gmail.com>
2024-09-30 15:34:35 -0400

    Completed client function using go channels

--- client/bi_stream.go
+++ client/bi_stream.go
@@ -1,7 +1,44 @@
 package main
 
 import (
+	"context"
+	"io"
+	"log"
+
 	pb "github.com/tejasvi541/Go-grpc/proto"
 )
 
-func callSayHelloBidirectionalStreaming(client pb.GreetServiceClient, names *pb.NamesList) {}
+func callSayHelloBidirectionalStreaming(client pb.GreetServiceClient, names *pb.NamesList) {
+	log.Println("Calling SayHelloBidirectionalStreaming")
+	stream, err := client.SayHelloBidirectionalStreaming(context.Background())
+
+	if err != nil {
+		log.Fatalf("Error while calling SayHelloBidirectionalStreaming: %v", err)
+	}
+
+	waitc := make(chan struct{})
+	go func() {
+		for {
+			res, err := stream.Recv()
+			if err == io.EOF {
+				close(waitc)
+				return
+			}
+			if err != nil {
+				log.Fatalf("Error while receiving message: %v", err)
+				return
+			}
+			log.Printf("Received message: %s", res.Message)
+		}
+	}()
+	go func() {
+		for _, name := range names.Names {
+			log.Printf("Sending message: %s", name)
+			if err := stream.Send(&pb.HelloRequest{Name: name}); err != nil {
+				log.Fatalf("Error while sending message: %v", err)
+			}
+		}
+		stream.CloseSend()
+	}()
+	<-waitc
+}
